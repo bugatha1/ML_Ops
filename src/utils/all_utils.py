c2d56d9453db6edecce38b923837e98ef6dd5641
bugatha1 <umasankar.bugatha@outlook.com>
2022-01-29 17:09:49 -0600

    stage_01_load_save file created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,9 +1,14 @@
 import yaml
 import os
 
-def read_yaml(path_to_file: str) -> dict:
+def read_config(path_to_file: str) -> dict:
     with open(path_to_file) as yaml_file:
-        content = yaml.safe_load(path_to_file)
+        content = yaml.safe_load(yaml_file)
         
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is create at {dir_path}")
 
