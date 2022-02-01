a3f3848b8e564a7de99d74dd499b4a28a51d57c7
bugatha1 <umasankar.bugatha@outlook.com>
2022-01-31 23:34:55 -0600

    stage 4 added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,10 +1,10 @@
 import yaml
 import os
+import json
 
 def read_config(path_to_file: str) -> dict:
     with open(path_to_file) as yaml_file:
-        content = yaml.safe_load(yaml_file)
-        
+        content = yaml.safe_load(yaml_file)        
     return content
 
 def create_directory(dirs: list):
@@ -16,3 +16,7 @@ def save_local_df(data, data_path, index_state=False):
     data.to_csv(data_path, index=index_state)
     print(f"data is save at {data_path}")
 
+def save_reprots(report: dict, report_path: str):
+    with open(report_path, 'w') as f:
+        json.dump(report, f, indent=4)
+    print(f"reports are saved at {report_path}")
